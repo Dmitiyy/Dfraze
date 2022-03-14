c69a558aef48f298a6c1a75a6049021ab54c8281
Dmitriy <dmitriyy311@gmail.com>
2022-03-14 16:36:29 +0200

    solved task

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,45 @@
-import { fromEvent, map, Subject } from "rxjs";
+import { fromEvent, map, take, Observable, Subject, switchMap } from "rxjs";
 
-// "example name" --> "ExampleName"
-// "your-NaMe-here" --> "YourNameHere"
-// "testing ABC" --> "TestingAbc"
-
-const data$: Subject<string[]> = new Subject();
 const btnElement = document.querySelector('.btn');
+const data$: Subject<string[]> = new Subject();
+const passedData: string = 'your-name-Here';
+let firstFinalResult: string = '';
 
+const isDash$: Observable<{active: Boolean, text: string}> = new Observable(subscriber => {
+  if (passedData.includes('-')) {
+    subscriber.next({active: true, text: passedData});
+  } else {
+    subscriber.next({active: false, text: firstFinalResult});
+  }
+});
 
 fromEvent(btnElement!, 'click')
-  .subscribe(() => {data$.next(['example name'])});
+  .subscribe(() => {data$.next([passedData])});
+
+const transformText = (text: string, active: Boolean): string => {
+  const stringData: string[] = text.split(!active ? ' ' : '-');
+  let result: string = '';
+
+  for (let item of stringData) {
+    if (item.length !== 0) {
+      const endOfWord: string = item.slice(1, item.length).toLowerCase();
+      result += item[0].toUpperCase() + endOfWord;
+    }
+  }
+
+  if (!active) {firstFinalResult = result};
+  return result;
+}
 
 data$
   .pipe(
-    map(data => {
-      const item: string = data[0];
-      const itemData: string[] = item.split(' ');
-      let firstFinalResult: string = '';
-
-      for (let stringElement of itemData) {
-        const bigLetter: string = stringElement[0].toUpperCase();
-
-        if (stringElement[0] !== bigLetter) {
-          firstFinalResult += bigLetter + stringElement.slice(1, stringElement.length);
-        }
-      }
-
-      return firstFinalResult;
-    }),
+    map(data => {return transformText(data[0], false)}),
+    switchMap(() => {return isDash$}),
+    take(1)
   ).subscribe(data => {
-    console.log(data);
+    let result: string = '';
+    if (data.active) {result = transformText(data.text, true)} 
+    
+    else {result = data.text};
+    document.querySelector('h3')!.textContent = result;
   });
